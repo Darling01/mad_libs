fd4a0b295f904dc41c4e04472c5c7eeff624a28f
Cbas <sg52373@gmail.com>
2023-07-06 19:30:43 -0600

    first commit

---
+++ historias_locas.py
@@ -0,0 +1,18 @@
+#concatenar cadenas de caracteres.
+#supongamos que queremos crear una cadena que diga:
+#aprende a programar con  ________.
+
+# organizacion = "freeCodeCamp"
+
+# print("aprende a programar con " + organizacion)
+# print("aprende a programar con {}".format(organizacion))
+# print(f"aprende a programar con {organizacion}") #f-string
+
+#Mad libs
+palabra1 = input()
+palabra2 = input()
+palabra3 = input()
+
+madlib = f"primer palabra {palabra1}, segunda palabra {palabra2}, tercera palabra {palabra3}"
+
+print(madlib)
